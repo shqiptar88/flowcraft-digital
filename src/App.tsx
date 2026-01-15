9b03e6a2837dff0cc8d855aaa1c44bc102f9bb7e
FlowCraft <kobeby2414@gmail.com>
2026-01-15 10:40:43 +0100

    fix routing, index page, bleaching subpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-// src/App.tsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 
-import Home from "./pages/Home";
-import BleachingLandingpageMuenchen from "./pages/beispiele/BleachingLandingpageMuenchen";
+import Index from "./pages/Index";
+import BleachingLandingpageMuenchen from "./pages/beispiele/bleachinglandingpage-muenchen";
 
 export default function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Index />} />
         <Route
           path="/beispiele/bleachinglandingpage-muenchen"
           element={<BleachingLandingpageMuenchen />}
