6e3f046fc09daf9d51b16e0a93abfe3a6386d91e
FlowCraft <kobeby2414@gmail.com>
2026-01-16 18:00:41 +0100

    Add Praxis Hamburg demo page

--- src/components/Demos/PraxisHamburg/IndexHamburg.tsx
+++ src/components/Demos/PraxisHamburg/IndexHamburg.tsx
@@ -1,37 +1,18 @@
-import { useEffect } from "react";
-
+// src/components/Demos/PraxisHamburg/IndexHamburg.tsx
 import HeaderHamburg from "./HeaderHamburg";
 import HeroHamburg from "./HeroHamburg";
-import USPCardsHamburg from "./USPCardsHamburg";
 import ServicesHamburg from "./ServicesHamburg";
-import ProcessHamburg from "./ProcessHamburg";
 import TeamHamburg from "./TeamHamburg";
-import ReviewsHamburg from "./ReviewsHamburg";
-import FAQHamburg from "./FAQHamburg";
 import ContactHamburg from "./ContactHamburg";
-import FooterHamburg from "./FooterHamburg";
 
 export default function IndexHamburg() {
-  useEffect(() => {
-    document.title = "Zahnarztpraxis Hamburg-Eppendorf | Demo";
-    // damit du beim Aufruf IMMER oben startest
-    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
-  }, []);
-
   return (
     <>
       <HeaderHamburg />
-      <main>
-        <HeroHamburg />
-        <USPCardsHamburg />
-        <ServicesHamburg />
-        <ProcessHamburg />
-        <TeamHamburg />
-        <ReviewsHamburg />
-        <FAQHamburg />
-        <ContactHamburg />
-      </main>
-      <FooterHamburg />
+      <HeroHamburg />
+      <ServicesHamburg />
+      <TeamHamburg />
+      <ContactHamburg />
     </>
   );
 }
