74ba49ffc9ee8f5f78e2a5ff7d6cb2d188dfc28f
FlowCraft <kobeby2414@gmail.com>
2026-01-17 20:29:25 +0100

    add import hero

--- src/pages/beispiele/praxis-hamburg.tsx
+++ src/pages/beispiele/praxis-hamburg.tsx
@@ -1,9 +1,11 @@
 import HeaderHamburg from "@/components/Demos/PraxisHamburg/HeaderHamburg";
+import HeroHamburg from "@/components/Demos/PraxisHamburg/HeroHamburg";
 
 export default function PraxisHamburgPage() {
   return (
     <>
       <HeaderHamburg />
+      <HeroHamburg />
     </>
   );
 }
