9c29a915b56c0a8809525996687621eafa7faa3d
FlowCraft <kobeby2414@gmail.com>
2026-01-17 20:32:41 +0100

    add import usp cards

--- src/pages/beispiele/praxis-hamburg.tsx
+++ src/pages/beispiele/praxis-hamburg.tsx
@@ -1,5 +1,6 @@
 import HeaderHamburg from "@/components/Demos/PraxisHamburg/HeaderHamburg";
 import HeroHamburg from "@/components/Demos/PraxisHamburg/HeroHamburg";
+import USPCardsHamburg from "@/components/Demos/PraxisHamburg/USPCardsHamburg";
 
 export default function PraxisHamburgPage() {
   return (
