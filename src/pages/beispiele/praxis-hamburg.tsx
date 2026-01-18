04b380bbe0abdc118a96d4ab4c849d3bd0db33ed
FlowCraft <kobeby2414@gmail.com>
2026-01-18 10:34:22 +0100

    add ServicesHamburg

--- src/pages/beispiele/praxis-hamburg.tsx
+++ src/pages/beispiele/praxis-hamburg.tsx
@@ -1,13 +1,16 @@
 import HeaderHamburg from "@/components/Demos/PraxisHamburg/HeaderHamburg";
 import HeroHamburg from "@/components/Demos/PraxisHamburg/HeroHamburg";
-import USPCardsHamburg from "@/components/Demos/PraxisHamburg/USPCardsHamburg";
+
+import ServicesHamburg from "@/components/Demos/PraxisHamburg/ServicesHamburg";
+
 
 export default function PraxisHamburgPage() {
   return (
     <>
       <HeaderHamburg />
       <HeroHamburg />
-      <USPCardsHamburg/>
+  
+      <ServicesHamburg />
     </>
   );
 }
